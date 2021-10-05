6d690b7a9bfd799a67c54d956213db795f319b45
YG-Dev <dawid8391@wp.pl>
2021-10-05 15:44:00 +0200

    mainbanner progress

--- src/Components/MainBanner.js
+++ src/Components/MainBanner.js
@@ -1,4 +1,4 @@
-import { Container } from 'react-bootstrap';
+import { Container, Button } from 'react-bootstrap';
 
 function MainBanner () {
     return (
@@ -6,6 +6,8 @@ function MainBanner () {
             <svg class="waves" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0353A4" fill-opacity="1" d="M0,192L40,186.7C80,181,160,171,240,176C320,181,400,203,480,202.7C560,203,640,181,720,165.3C800,149,880,139,960,154.7C1040,171,1120,213,1200,245.3C1280,277,1360,299,1400,309.3L1440,320L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path></svg>
             <Container align="center">
                 <h1 className="company-title">Company</h1>
+                <h2 className="company-quote">Put motivational quote <strong>here</strong></h2>
+                <Button size="lg">Contact us</Button>
             </Container>
         </Container>
     )
--- src/Components/MenuNavbar.js
+++ src/Components/MenuNavbar.js
@@ -7,15 +7,15 @@ function MenuNavbar () {
         <header>
             <Navbar expand="lg" className="menu-navbar">
             <Container>
-                <Navbar.Brand><h2><img src={logo} alt="YG Dev"/>YG Dev</h2></Navbar.Brand>
+                <Navbar.Brand href="#"><img src={logo} alt="YG Dev" className="logo-img"/></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="ms-auto menu-elements"> 
-                    <Nav.Link className="menu-single-element ms-3">Strona główna</Nav.Link>
-                    <Nav.Link className="menu-single-element ms-3">O nas</Nav.Link>
-                    <Nav.Link className="menu-single-element ms-3">Usługi</Nav.Link>
+                    <Nav.Link className="menu-single-element ms-3">Home page</Nav.Link>
+                    <Nav.Link className="menu-single-element ms-3">About us</Nav.Link>
+                    <Nav.Link className="menu-single-element ms-3">Services</Nav.Link>
                     <Nav.Link className="menu-single-element ms-3">Portfolio</Nav.Link>
-                    <Nav.Link className="menu-single-element ms-3">Kontakt</Nav.Link>
+                    <Nav.Link className="menu-single-element ms-3">Contact</Nav.Link>
                 </Nav>
                 </Navbar.Collapse>
             </Container>
